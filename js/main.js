69ee587303f861e05b2a7d4989ff0c57d93242ad
unknown <marciokennedy2002@gmail.com>
2021-01-17 19:47:15 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 4000
+});
